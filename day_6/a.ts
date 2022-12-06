8bc9b913c6a74c041a5fcb84b4539b78ef2cd019
y.tsvyatkov <yoantsvyatkov@ocado.com>
2022-12-06 21:48:38 +0200

    day 5 and day 6

---
+++ day_6/a.ts
@@ -0,0 +1,17 @@
+const text = await Deno.readTextFile("./input.txt")
+const areUnique = (str: string, start: number, end: number) => {
+    for (let i = start; i < end; i++) {
+        for(let j = i + 1; j <= end; j++) {
+           if (str[i] === str[j]) {
+            return false;
+           } 
+        }
+    }
+    return true;
+}
+for (let i = 3; i < text.length; i++) {
+    if (areUnique(text, i - 3, i)) {
+        console.log(i + 1);
+        break;
+    }
+}
